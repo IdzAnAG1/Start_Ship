be2d76a98b1e4a0e95ca6d33b7c9b9bc1585ed80
IdzAnAG1 <gusyackovegor@yandex.com>
2024-12-12 13:37:48 +0300

    Added ErrorDescription for castom error messages and ModulesHandlers to support single/multiple module proccesing

--- ErrorDescription/Errors.go
+++ ErrorDescription/Errors.go
@@ -1,7 +1,9 @@
 package ErrorDescription
 
 var (
-	InvalidEncode string = "Ошибка при получении данных %v "
-	InvalidDecode string = "Ошибка при записи данных %v "
-	InvalidMethod string = "Ошибка получения доступа к методу"
+	InvalidEncode  string = "Ошибка при получении данных %v "
+	InvalidDecode  string = "Ошибка при записи данных %v "
+	InvalidMethod  string = "Ошибка получения доступа к методу"
+	InvalidID      string = "Ошибка получения доступа к ID"
+	ObjectNotFound string = "Объект не найден"
 )
--- handlers/ModulesHandler.go
+++ handlers/ModulesHandler.go
@@ -7,6 +7,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func ModulesHandler(w http.ResponseWriter, r *http.Request) {
@@ -32,6 +33,62 @@ func ModulesHandler(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set(variables.ContentType, variables.ApplicationJSON)
 			w.WriteHeader(http.StatusCreated)
 			json.NewEncoder(w).Encode(module)
+		}
+	default:
+		http.Error(w, ErrorDescription.InvalidMethod, http.StatusMethodNotAllowed)
+	}
+}
+
+func ModuleByIDHandler(w http.ResponseWriter, r *http.Request) {
+	idString := r.URL.Path[len("/modules/"):]
+	id, err := strconv.Atoi(idString)
+	if err != nil {
+		http.Error(w, ErrorDescription.InvalidID, http.StatusBadRequest)
+	}
+	switch r.Method {
+	case http.MethodGet:
+		{
+			for _, module := range variables.Modules {
+				if module.ID == id {
+					w.Header().Set(variables.ContentType, variables.ApplicationJSON)
+					json.NewEncoder(w).Encode(module)
+					return
+				}
+			}
+			http.Error(w, ErrorDescription.ObjectNotFound, http.StatusNotFound)
+		}
+	case http.MethodPut:
+		{
+			var updatedModule structures.Module
+			err := json.NewDecoder(r.Body).Decode(&updatedModule)
+			if err != nil {
+				http.Error(w, ErrorDescription.InvalidDecode, http.StatusBadRequest)
+			}
+
+			for i, module := range variables.Modules {
+				if module.ID == id {
+					variables.Modules[i].Name = updatedModule.Name
+					variables.Modules[i].Status = updatedModule.Status
+					variables.Modules[i].Power = updatedModule.Power
+					w.Header().Set(variables.ContentType, variables.ApplicationJSON)
+					err = json.NewEncoder(w).Encode(variables.Modules[i])
+					if err != nil {
+						http.Error(w, ErrorDescription.InvalidEncode, http.StatusBadRequest)
+					}
+					return
+				}
+			}
+			http.Error(w, ErrorDescription.ObjectNotFound, http.StatusNotFound)
+		}
+	case http.MethodDelete:
+		{
+			for i, module := range variables.Modules {
+				if module.ID == id {
+					variables.Modules = append(variables.Modules[:i], variables.Modules[i:]...)
+					w.WriteHeader(http.StatusNoContent)
+				}
+			}
+			http.Error(w, ErrorDescription.ObjectNotFound, http.StatusNotFound)
 		}
 	default:
 		http.Error(w, ErrorDescription.InvalidMethod, http.StatusMethodNotAllowed)
